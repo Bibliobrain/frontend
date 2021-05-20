c4f111a2e6563e1072b5223ba4ec0b13057ac400
michal-cic <michal.cic@gmail.com>
2021-05-20 18:59:46 +0200

    added loan returning

--- src/requests/LoanService.js
+++ src/requests/LoanService.js
@@ -1,29 +1,29 @@
-import request from './Request';
+import request from "./Request";
 
 const LoanService = {
-    loan: (copyId, memberId) => {
-        return request({
-            url: `/loans`,
-            method: 'POST',
-            body:{
-                copyId,
-                memberId
-            }
-        })
-    },
+  loan: (copyId, memberId) => {
+    return request({
+      url: `/loans`,
+      method: "POST",
+      body: {
+        copyId,
+        memberId,
+      },
+    });
+  },
 
-    return: (copyId, memberId, condition, borrowDate) => {
-        return request({
-            url: `/loans/return`,
-            method: 'POST',
-            body:{
-                copyId,
-                memberId,
-                condition, 
-                borrowDate
-            }
-        })
-    }
-}
+  return: (copyId, memberId, condition, borrowDate) => {
+    return request({
+      url: `/loans/return`,
+      method: "POST",
+      body: {
+        copyId,
+        memberId,
+        condition,
+        borrowDate,
+      },
+    });
+  },
+};
 
 export default LoanService;
