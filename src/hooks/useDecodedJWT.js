9ca5d1b13534abfa1dc98306a1b04bc1677d6e2d
Marek Vargovcik <vargovcik.marek@gmail.com>
2021-05-22 00:15:29 +0200

    implemented core tests

--- src/hooks/useDecodedJWT.js
+++ src/hooks/useDecodedJWT.js
@@ -1,15 +1,10 @@
-import { useMemo } from 'react';
-
 import { parseJWT } from '../utils/parseJWT';
 
 import { useAuth } from './stores/useAuth';
 
 const useDecodedJWT = () => {
   const refreshToken = useAuth((state) => state.refreshToken);
-  return useMemo(
-    () => (refreshToken ? parseJWT(refreshToken) : {}),
-    [refreshToken]
-  );
+  return refreshToken ? parseJWT(refreshToken) : {};
 };
 
 export { useDecodedJWT };
